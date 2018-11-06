ada6138868b274f4eb0f4e07f80647664a207127
ItRealRew <31623137+ItRealRew@users.noreply.github.com>
2018-11-06 20:21:49 +0300

    12 tango
    12 tango (C) ?

---
+++ app/src/main/java/com/example/nik_ax/androidlab8camera/MainActivity.java
@@ -0,0 +1,188 @@
+package com.example.nik_ax.androidlab8camera;
+
+
+import android.graphics.Matrix;
+import android.graphics.RectF;
+import android.hardware.Camera;
+import android.hardware.Camera.CameraInfo;
+import android.hardware.Camera.Size;
+import android.media.MediaRecorder;
+import android.os.Bundle;
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Display;
+import android.view.Surface;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+
+import java.io.File;
+import java.io.FileOutputStream;
+
+public class MainActivity extends AppCompatActivity {
+    final int CAMERA_ID = 0;
+    final boolean FULL_SCREEN = false;
+    private SurfaceView surfaceView;
+    private Camera camera;
+    private MediaRecorder mediaRecorder;
+    private SurfaceHolder holder;
+    File photoFile;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        File pictures = Environment
+                .getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+        photoFile = new File(pictures, "photo_lab8.jpg");
+        surfaceView = (SurfaceView) findViewById(R.id.surfaceView);
+        holder = surfaceView.getHolder();
+        //  holder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+
+
+        holder.addCallback(new SurfaceHolder.Callback() {
+            @Override
+            public void surfaceCreated(SurfaceHolder holder) {
+                try {
+                    camera.setPreviewDisplay(holder);
+                    camera.startPreview();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void surfaceChanged(SurfaceHolder holder, int format,
+                                       int width, int height) {
+                camera.stopPreview();
+                setCameraDisplayOrientation(CAMERA_ID);
+                try {
+                    camera.setPreviewDisplay(holder);
+                    camera.startPreview();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void surfaceDestroyed(SurfaceHolder holder) {
+            }
+        });
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        camera = Camera.open(CAMERA_ID);
+        setPreviewSize(FULL_SCREEN);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (camera != null)
+            camera.release();
+        camera = null;
+    }
+    void setPreviewSize(boolean fullScreen) {
+
+        // получаем размеры экрана
+        Display display = getWindowManager().getDefaultDisplay();
+        boolean widthIsMax = display.getWidth() > display.getHeight();
+
+        // определяем размеры превью камеры
+        Size size = camera.getParameters().getPreviewSize();
+
+        RectF rectDisplay = new RectF();
+        RectF rectPreview = new RectF();
+
+        // RectF экрана, соотвествует размерам экрана
+        rectDisplay.set(0, 0, display.getWidth(), display.getHeight());
+
+        // RectF первью
+        if (widthIsMax) {
+            // превью в горизонтальной ориентации
+            rectPreview.set(0, 0, size.width, size.height);
+        } else {
+            // превью в вертикальной ориентации
+            rectPreview.set(0, 0, size.height, size.width);
+        }
+
+        Matrix matrix = new Matrix();
+        // подготовка матрицы преобразования
+        if (!fullScreen) {
+            // если превью будет "втиснут" в экран (второй вариант из урока)
+            matrix.setRectToRect(rectPreview, rectDisplay,
+                    Matrix.ScaleToFit.START);
+        } else {
+            // если экран будет "втиснут" в превью (третий вариант из урока)
+            matrix.setRectToRect(rectDisplay, rectPreview,
+                    Matrix.ScaleToFit.START);
+            matrix.invert(matrix);
+        }
+        // преобразование
+        matrix.mapRect(rectPreview);
+
+        // установка размеров surface из получившегося преобразования
+        surfaceView.getLayoutParams().height = (int) (rectPreview.bottom);
+        surfaceView.getLayoutParams().width = (int) (rectPreview.right);
+    }
+    void setCameraDisplayOrientation(int cameraId) {
+// определяем насколько повернут экран от нормального положения
+        int rotation = getWindowManager().getDefaultDisplay().getRotation();
+        int degrees = 0;
+        switch (rotation) {
+            case Surface.ROTATION_0:
+                degrees = 0;
+                break;
+            case Surface.ROTATION_90:
+                degrees = 90;
+                break;
+            case Surface.ROTATION_180:
+                degrees = 180;
+                break;
+            case Surface.ROTATION_270:
+                degrees = 270;
+                break;
+        }
+
+        int result = 0;
+
+// получаем инфо по камере cameraId
+        CameraInfo info = new CameraInfo();
+        Camera.getCameraInfo(cameraId, info);
+
+// задняя камера
+        if (info.facing == CameraInfo.CAMERA_FACING_BACK) {
+            result = ((360 - degrees) + info.orientation);
+        } else
+// передняя камера
+            if (info.facing == CameraInfo.CAMERA_FACING_FRONT) {
+                result = ((360 - degrees) - info.orientation);
+                result += 360;
+            }
+        result = result % 360;
+        camera.setDisplayOrientation(result);
+    }
+
+
+    public void makePhoto(View view) {
+        camera.takePicture(null, null, new Camera.PictureCallback() {
+            @Override
+            public void onPictureTaken(byte[] data, Camera camera) {
+                try {
+                    FileOutputStream fos = new FileOutputStream(photoFile);
+                    fos.write(data);
+                    fos.close();
+                    camera.stopPreview();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+    }
+
+
+}
